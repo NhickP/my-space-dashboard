9206a103ec36ad868fe870804728312d2ece236b
NhickP <nickjai49@gmail.com>
2025-07-07 14:25:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🌌 Welcome to My Space Dashboard</h1>
+      <p>This is a minimal working starter for your space science project.</p>
+    </div>
+  );
+}
