30f46b0a3accf60423123dc99253ccfc3ffe96d0
NhickP <nickjai49@gmail.com>
2025-07-07 13:57:27 +0100

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Space Dashboard 🚀</h1>
+      <p>This is your starter screen. Ready to build?</p>
+    </div>
+  );
+}
