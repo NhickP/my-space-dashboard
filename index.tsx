81a0d5ec19387f14acdc17faf2ea19c20b408ad0
NhickP <nickjai49@gmail.com>
2025-07-07 14:02:30 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,1 @@
 
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to My Space Dashboard 🚀</h1>
-      <p>This is your starter screen. Ready to build?</p>
-    </div>
-  );
-}
