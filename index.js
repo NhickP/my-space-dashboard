a6a60d07c67b8a80142e29d65e7ccd7f211fee9b
NhickP <nickjai49@gmail.com>
2025-07-07 14:44:21 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>🌌 Welcome to My Space Dashboard</h1>
-      <p>This is a minimal working starter for your space science project.</p>
-    </div>
-  );
-}
